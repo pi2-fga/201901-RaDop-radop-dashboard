9ec1db3cc9e78cb680fe3f575d55db0256e9216d
marimendes <mare.s.mendes@gmail.com>
2019-05-09 20:35:18 -0300

    Adding new fields on user model

--- dashboard/users/models.py
+++ dashboard/users/models.py
@@ -1,3 +1,4 @@
+from django.db import models
 from django.contrib.auth.models import AbstractUser
 from django.db.models import CharField
 from django.urls import reverse
@@ -8,7 +9,39 @@ class User(AbstractUser):
 
     # First Name and Last Name do not cover name patterns
     # around the globe.
-    name = CharField(_("Name of User"), blank=True, max_length=255)
+    name = CharField(_("Nome do usuário"), blank=True, max_length=255)
+
+    gender = models.CharField(
+        _('Gender'),
+        choices=(
+            (("Masculino"), _("Masculino")),
+            (("Feminino"), _("Feminino")),
+        ),
+        blank=False,
+        max_length=10,
+        null=True
+    )
+
+    birthday = models.DateField(
+        _('Birthday'),
+        help_text=_("xx/xx/xxxx"),
+        blank=False,
+        null=True
+    )
+
+    cpf = models.CharField(
+        help_text=_("Por favor, entre com um CPF" +
+                    "seguindo o formato: XXX.XXX.XXX-XX"),
+        unique=True,
+        max_length=14,
+        default='000.000.000-00',
+    )
+
+    matricula = models.CharField(
+        unique=True,
+        max_length=7,
+        default='00'
+    )
 
     def get_absolute_url(self):
         return reverse("users:detail", kwargs={"username": self.username})
